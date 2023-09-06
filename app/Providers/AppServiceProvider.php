1c515430ad5d154f79d785a30aa10f0627605134
Bassem272 <73647090+Bassem272@users.noreply.github.com>
2023-09-06 05:08:48 +0300

    gt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Rules\OrderItemsRule;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         // Register your custom validation rule here
+         Validator::extend('order_items_rule', function ($attribute, $value, $parameters, $validator) {
+            // Here you can call your custom validation logic from the OrderItemsRule class
+            $orderItemsRule = new OrderItemsRule();
+            return $orderItemsRule->passes($attribute, $value);
+        });
+
+        // Other code in the boot method...
     }
 }
